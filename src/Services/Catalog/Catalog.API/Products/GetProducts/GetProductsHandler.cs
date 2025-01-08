74a37e4174c92c7b7de251e7098b1e571ac2e980
mrapaic9 <rapaicm9@gmail.com>
2025-01-08 15:15:23 +0100

    Developed GET ALL products endpoint and cleaned up Handler class

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,17 +1,17 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
 public record GetProductsQuery() : IQuery<GetProductsResult>;
-public record GetProductsResult(IEnumerable<Product> Products)
-{
-    internal class GetProductsQueryHandler
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler
         (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
         : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
-        {
-            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
-            return new GetProductsResult(products);
-        }
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
     }
 }
+
