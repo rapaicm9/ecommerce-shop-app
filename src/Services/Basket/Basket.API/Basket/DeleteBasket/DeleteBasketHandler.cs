8cede56af6c176379945c806f4386884f8b4abf5
mrapaic9 <rapaicm9@gmail.com>
2025-01-15 16:57:43 +0100

    Added logic to DeleteBasket Feature Handler and Endpoint classes

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,25 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required!");
+        }
+    }
+
+    public class DeleteBasketCommandHandler
+        : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: Delete basket from DB and Cache
+            // session.Delete<ShoppingCart>(command.Id);
+
+            return new DeleteBasketResult(true);    
+        }
     }
 }
