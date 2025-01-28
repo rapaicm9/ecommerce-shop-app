0da68a4841c037d416e098e53be267aceb64a6bb
mrapaic9 <rapaicm9@gmail.com>
2025-01-28 02:07:27 +0100

    Finished set up of Ordering.API microservice

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            // TODO: Add services related to Carter and other API related libraries
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication webApplication)
+        {
+            // TODO: Lifecycle goes here
+
+            return webApplication;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // TODO: Add services related to EF Core and DB Context
+
+            return services;
+        }
+    }
+}
