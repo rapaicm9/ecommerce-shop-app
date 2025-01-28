f39d61b8eb7fba869e1d99fcdcb814b1a38593b6
mrapaic9 <rapaicm9@gmail.com>
2025-01-28 04:33:14 +0100

    Completed Ordering.Domain layer with all the necessary classes and logic

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,35 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string ExpirationDate { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+
+        protected Payment()
+        {
+
+        }
+
+        private Payment(string cardName, string cardNumber, string expirationDate, string cvv, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            ExpirationDate = expirationDate;
+            CVV = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expirationDate, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName,cardNumber,expirationDate, cvv, paymentMethod);
+        }
+    }
+}
