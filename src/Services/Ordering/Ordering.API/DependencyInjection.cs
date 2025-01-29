73e4d51d855843bf8ec966c4d3c5a6b366fb57fc
mrapaic9 <rapaicm9@gmail.com>
2025-01-29 01:13:31 +0100

    Completed development of Ordering.API layer with all necessary classes and logic (Endpoints)

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,17 +1,34 @@
-﻿namespace Ordering.API
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration cfg)
         {
-            // TODO: Add services related to Carter and other API related libraries
+            services.AddCarter();
+
+            services.AddExceptionHandler<CustomExceptionHandler>();
+
+            services.AddHealthChecks().AddSqlServer(cfg.GetConnectionString("Database")!);
 
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication webApplication)
         {
-            // TODO: Lifecycle goes here
+            webApplication.MapCarter();
+
+            webApplication.UseExceptionHandler(opts => { });
+
+            webApplication.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return webApplication;
         }
---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,31 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.DTOs;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints;
+public record CreateOrderRequest(OrderDTO Order);
+public record CreateOrderResponse(Guid Id);
+
+public class CreateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateOrderResponse>();
+
+            return Results.Created($"/orders/{response.Id}", response);
+        })
+        .WithName("CreateOrder")
+        .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Order")
+        .WithDescription("Create Order");
+    }
+}
